b20215ae43e9eb0246993cd827df38ec60471c2e
Brandon Shoop <brandon.shoop@boats.com>
2020-12-28 12:07:55 -0500

    [feat-constants] Added constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,8 @@
-/*
-BOOK_RSS=https://www.goodreads.com/review/list_rss/{ID}?shelf=to-read
-BOOK_BASE=https://www.goodreads.com/book/show/
-GOOGLE_PREVIEW_BASE=http://webcache.googleusercontent.com/search?q=cache:
-*/
-
-const BOOK_RSS: string =
+export const BOOK_RSS: string =
   'https://www.goodreads.com/review/list_rss/{ID}?shelf=to-read';
-const BOOK_BASE: string = 'https://www.goodreads.com/book/show/';
+
+export const BOOK_BASE: string = 'https://www.goodreads.com/book/show/';
+
+export const GOOGLE_PREVIEW_BASE: string =
+  'http://webcache.googleusercontent.com/search?q=cache:';
+
