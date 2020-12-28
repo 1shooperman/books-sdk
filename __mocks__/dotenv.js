038476d26f16ab09db1db74564f60d679ec1f35a
Brandon Shoop <brandon.shoop@boats.com>
2020-12-28 09:55:45 -0500

    [feat-constants] Removed dotenv

--- __mocks__/dotenv.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  config: () => {
-    process.env.BOOK_RSS = 'https://fake.url1';
-    process.env.BOOK_BASE = 'https://fake.url2';
-    process.env.GOOGLE_PREVIEW_BASE = 'http://fake.url3';
-  },
-};
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+/*
+BOOK_RSS=https://www.goodreads.com/review/list_rss/{ID}?shelf=to-read
+BOOK_BASE=https://www.goodreads.com/book/show/
+GOOGLE_PREVIEW_BASE=http://webcache.googleusercontent.com/search?q=cache:
+*/
+
+const BOOK_RSS: string =
+  'https://www.goodreads.com/review/list_rss/{ID}?shelf=to-read';
+const BOOK_BASE: string = 'https://www.goodreads.com/book/show/';
